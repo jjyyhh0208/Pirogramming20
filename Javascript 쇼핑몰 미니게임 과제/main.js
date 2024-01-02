af672871f4d89261c84fd905c31acc320cb9722f
Github <Github>
2024-01-03 02:08:01 +0900

    1-JavaScript-display items

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -4,10 +4,23 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+function createHTMLString(item) {
+  return `
+  <li class="item">
+      <img src="${item.image}" alt= "${item.type}" class="item_thumbnail" />
+      <span class="item_description">${item.gender}, ${item.size}</span>
+  </li>
+  `;
+}
+
 loadItems()
   .then((items) => {
-    console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventListeners(items);
   })
   .catch(console.log);

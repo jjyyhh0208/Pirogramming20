0ffcd33f0d4fbc353bf03e9bac191d3ca2534a3a
Github <jjyyhh0208@korea.ac.kr>
2024-01-09 23:34:27 +0900

    Django 템플릿

--- djanggogirls/blog/models.py
+++ djanggogirls/blog/models.py
@@ -6,9 +6,9 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_data = models.DateTimeField(
+    created_date = models.DateTimeField(
             default=timezone.now)
-    published_data = models.DateTimeField(
+    published_date = models.DateTimeField(
             blank=True, null=True)
     
     def publish(self):
